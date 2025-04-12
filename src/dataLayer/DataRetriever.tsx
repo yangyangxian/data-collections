45ee2002f2a8be1c2edce947bda18f6e48862c3f
Yang Yang <persistence@vip.qq.com>
2025-04-12 11:50:08 +0800

    Added more animal data

--- src/dataLayer/DataRetriever.tsx
+++ src/dataLayer/DataRetriever.tsx
@@ -47,7 +47,27 @@ export const getAnimals = (): Array<Animal> => {
         new Animal('12', 'Lion狮子', 'Panthera豹属', 'Felidae猫科',
             'Panthera leo', 80, '/images/animals/lion.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
         new Animal('13', 'Hare野兔', 'Lepus兔属', 'Leporidae兔科',
-            'Lepus timidus', 80, '/images/animals/hare.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'));
+            'Lepus timidus', 80, '/images/animals/hare.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('14', 'African wild dog非洲野犬', 'Lycaon非洲野犬属', 'Canidae犬科',
+            'Lycaon pictus', 71, '/images/animals/africanwilddog.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('15', 'Red kangaroo红大袋鼠', 'Macropus大袋鼠属', 'Macropodidae袋鼠科',
+            'Osphranter rufus', 71, '/images/animals/kangaroo.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('16', 'Greyhound格雷伊猎犬', 'Canis犬属', 'Canidae犬科',
+            'Canis lupus familiaris', 70, '/images/animals/greyhound.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('17', 'Onager亚洲野驴', 'Equus马属', 'Equidae马科',
+            'Equus hemionus', 70, '/images/animals/onager.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('18', 'Plains zebra平原斑马', 'Equus马属', 'Equidae马科',
+            'Equus quagga', 70, '/images/animals/zebra.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('19', 'Eland伊兰羚羊', 'Taurotragus巨羚属', 'Bovidae牛科',
+            'Taurotragus oryx', 70, '/images/animals/eland.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('20', 'Coyote土狼', 'Canis犬属', 'Canidae犬科',
+            'Canis latrans', 65, '/images/animals/coyote.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('21', 'Tiger老虎', 'Panthera豹属', 'Felidae猫科',
+            'Panthera tigris', 64, '/images/animals/tiger.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+        new Animal('22', 'Spotted hyena斑鬣狗', 'Crocuta斑鬣狗属', 'Hyaenidae鬣狗科',
+            'Crocuta crocuta', 60, '/images/animals/spottedhyena.jpeg', 'https://en.wikipedia.org/wiki/Fastest_animals'),
+
+        );
            
     return animalList;
 };
