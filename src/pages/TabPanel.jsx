fb5a34b546c2de9fa24e76906ea2c568649be841
Yang Yang <persistence@vip.qq.com>
2025-05-14 20:33:24 +0800

    link selected tab and route

--- src/pages/TabPanel.jsx
+++ src/pages/TabPanel.jsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 
 import PhotoWall from './PhotoWall.jsx';
 import CountryPage from './CountryPage.jsx';
@@ -15,25 +14,37 @@ console.debug("this is from top level of TabPanel.jsx");
 export default function BasicTabs() {
     console.debug("this is from top level of TabPanel function component");
     const navigate = useNavigate();
+    const location = useLocation();
     const [selectedTab, setSelectedTab] = React.useState(0);
 
+    // Map routes to tab indices
+    const routeToTabIndex = {
+        '/animals': 0,
+        '/countries': 1,
+        '/photo-wall': 2,
+        '/cars': 3,
+    };
+
+    // Update selectedTab based on the current route
+    React.useEffect(() => {
+        const currentTab = routeToTabIndex[location.pathname] ?? 0; // Default to 0 if route is not found
+        setSelectedTab(currentTab);
+    }, [location.pathname]);
+
     const handleChange = (event, newValue) => {
+        setSelectedTab(newValue);
         switch (newValue) {
             case 0:
                 navigate('/animals');
-                setSelectedTab(0);
                 break;
             case 1:
                 navigate('/countries');
-                setSelectedTab(1);
                 break;
             case 2:
                 navigate('/photo-wall');
-                setSelectedTab(2);
                 break;
             case 3:
                 navigate('/cars');
-                setSelectedTab(3);
                 break;
             default:
                 break;
@@ -44,13 +55,13 @@ export default function BasicTabs() {
         <Box>
             <Box sx={{ borderBottom: 1, borderColor: 'divider', overflowX: 'auto', whiteSpace: 'nowrap' }}>
                 <Tabs
-                    value={selectedTab} // Disable default tab selection
+                    value={selectedTab}
                     onChange={handleChange}
                     variant="scrollable"
                     indicatorColor="secondary"
                     textColor="inherit"
                     scrollButtons="auto"
-                    allowScrollButtonsMobile                    
+                    allowScrollButtonsMobile
                 >
                     <Tab label="Animals" />
                     <Tab label="Countries" />
@@ -63,6 +74,7 @@ export default function BasicTabs() {
                 <Route path="/countries" element={<CountryPage />} />
                 <Route path="/photo-wall" element={<PhotoWall />} />
                 <Route path="/cars" element={<CarPage />} />
+                <Route path="*" element={<AnimalPage />} /> {/* Default route */}
             </Routes>
         </Box>
     );
